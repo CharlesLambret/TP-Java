a761f54da0addb1843ec1d05d0c60eebc64b5b6c
Charles Lambret <charleslambretpro@gmail.com>
2024-04-12 16:08:36 +0200

    bean main

--- src/main/java/org/charleslambret/operateur/Main.java
+++ src/main/java/org/charleslambret/operateur/Main.java
@@ -1,24 +1,30 @@
 package org.charleslambret.operateur;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.util.List;
 import java.sql.SQLException;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String implementation = System.getProperty("reader.implementation", "FILE");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        
         try {
+            String implementation = System.getProperty("reader.implementation", "FILE");
             if (implementation.equals("JDBC")) {
-                DatabaseManager dbManager = new DatabaseManager();
+                DatabaseManager dbManager = context.getBean("databaseManager", DatabaseManager.class);
                 List<OperationData> operations = dbManager.fetchOperationData();
                 String outputDirectory = System.getProperty("user.dir");
                 ProcesseurFichiers.processOperations(operations, outputDirectory);
             } else {
                 String dirPath = args.length == 1 ? args[0] : System.getProperty("user.dir");
-                ProcesseurFichiers.processDirectory(dirPath);
+                ProcesseurFichiers processeur = context.getBean("processeurFichiers", ProcesseurFichiers.class);
+                processeur.processDirectory(dirPath);
             }
         } catch (SQLException | IOException e) {
             System.err.println("Erreur: " + e.getMessage());
+        } finally {
+            context.close();
         }
     }
 }
