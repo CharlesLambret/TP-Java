2866ee4f21687962fdd161d4c3a76f5207359726
Charles Lambret <charleslambretpro@gmail.com>
2024-04-12 16:10:24 +0200

    init beans dbmanager et processeurfichiers

--- src/main/java/org/charleslambret/operateur/Main.java
+++ src/main/java/org/charleslambret/operateur/Main.java
@@ -7,15 +7,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        
-        try {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
             String implementation = System.getProperty("reader.implementation", "FILE");
             if (implementation.equals("JDBC")) {
                 DatabaseManager dbManager = context.getBean("databaseManager", DatabaseManager.class);
                 List<OperationData> operations = dbManager.fetchOperationData();
                 String outputDirectory = System.getProperty("user.dir");
-                ProcesseurFichiers.processOperations(operations, outputDirectory);
+                ProcesseurFichiers processeur = context.getBean("processeurFichiers", ProcesseurFichiers.class);
+                processeur.processOperations(operations, outputDirectory);
             } else {
                 String dirPath = args.length == 1 ? args[0] : System.getProperty("user.dir");
                 ProcesseurFichiers processeur = context.getBean("processeurFichiers", ProcesseurFichiers.class);
@@ -23,8 +22,6 @@ public class Main {
             }
         } catch (SQLException | IOException e) {
             System.err.println("Erreur: " + e.getMessage());
-        } finally {
-            context.close();
         }
     }
 }
