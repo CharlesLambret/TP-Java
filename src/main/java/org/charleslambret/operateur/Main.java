96843bc9dc1d31ada135574b6a2e1199e2156009
Charles Lambret <charleslambretpro@gmail.com>
2024-04-12 10:07:33 +0200

    implémentation if else dans le main pour gérer les deux cas

--- src/main/java/org/charleslambret/operateur/Main.java
+++ src/main/java/org/charleslambret/operateur/Main.java
@@ -1,19 +1,24 @@
 package org.charleslambret.operateur;
-import java.util.List; 
+
+import java.util.List;
 import java.sql.SQLException;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseManager dbManager = new DatabaseManager();
+        String implementation = System.getProperty("reader.implementation", "FILE"); // Default to FILE
         try {
-            List<OperationData> operations = dbManager.fetchOperationData();
-            String outputDirectory = System.getProperty("user.dir"); 
-            ProcesseurFichiers.processOperations(operations, outputDirectory);
-        } catch (SQLException e) {
-            System.err.println("Erreur lors de la récupération des données : " + e.getMessage());
-        } catch (IOException e) {
-            System.err.println("Erreur I/O : " + e.getMessage());
+            if (implementation.equals("JDBC")) {
+                DatabaseManager dbManager = new DatabaseManager();
+                List<OperationData> operations = dbManager.fetchOperationData();
+                String outputDirectory = System.getProperty("user.dir");
+                ProcesseurFichiers.processOperations(operations, outputDirectory);
+            } else {
+                String dirPath = args.length == 1 ? args[0] : System.getProperty("user.dir");
+                ProcesseurFichiers.processDirectory(dirPath);
+            }
+        } catch (SQLException | IOException e) {
+            System.err.println("Erreur: " + e.getMessage());
         }
     }
 }
