6774a0c5ac2b8e53824aaf4de9744c597c7b823d
Charles Lambret <charleslambretpro@gmail.com>
2024-04-12 10:25:42 +0200

    rajout tests unitaires

---
+++ src/test/java/org/charleslambret/operateur/MainDatabaseTests.java
@@ -0,0 +1,53 @@
+
+package org.charleslambret.operateur;
+
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainDatabaseTests {
+
+    @Test
+    void testDatabaseConnection() {
+        String url = "jdbc:postgresql://SG-hetic-mt4-java-5275-pgsql-master.servers.mongodirector.com:5432/TP";
+        String user = "etudiant";
+        String password = "MT4@hetic2324";
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            assertNotNull(connection);
+        } catch (SQLException e) {
+            fail("Should have connected to the database");
+        }
+    }
+
+    @Test
+    void testFetchOperationData() {
+        DatabaseManager dbManager = new DatabaseManager();
+        try {
+            List<OperationData> operations = dbManager.fetchOperationData();
+            assertNotNull(operations);
+            assertFalse(operations.isEmpty()); // Ensure data is fetched
+        } catch (SQLException e) {
+            fail("Failed to fetch data from the database");
+        }
+    }
+
+    @Test
+    void testProcessOperations() {
+        DatabaseManager dbManager = new DatabaseManager();
+        try {
+            List<OperationData> operations = dbManager.fetchOperationData();
+            assertNotNull(operations);
+            String directory = System.getProperty("user.dir");
+
+            ProcesseurFichiers.processOperations(operations, directory);
+
+
+        } catch (SQLException | IOException e) {
+            fail("Processing operations failed due to " + e.getMessage());
+        }
+    }
+}
